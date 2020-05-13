d89b582c285b2ab74a478ac669da2463d4344a52
vonnenaut <danielashcom@gmail.com>
2020-05-13 09:12:30 +0700

    u/d routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root 'welcome#index'
 end
   
