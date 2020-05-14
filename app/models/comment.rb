8c3c79b1ff7136d84cda8cc341f56dcc15ef90f8
vonnenaut <danielashcom@gmail.com>
2020-05-14 11:08:23 +0700

    continuing to refactor, added delete functionality to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  validates :commenter, presence: true,
+                    length: { minimum: 3 }
+  validates :body, presence: true,
+                    length: { minimum: 3 }
 end
