501a84e26b9612eb8a71e0275f14388e342fb34b
vonnenaut <danielashcom@gmail.com>
2020-05-14 11:11:21 +0700

    reverted refactoring to comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,3 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :commenter, presence: true,
-                    length: { minimum: 3 }
-  validates :body, presence: true,
-                    length: { minimum: 3 }
 end
