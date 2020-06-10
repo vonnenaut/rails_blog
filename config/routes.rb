4a17c6eac7e47f33cf29e821aefca870dc580abb
vonnenaut <danielashcom@gmail.com>
2020-06-10 20:35:40 +0700

    Left off at 5.11, updating articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'articles/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'articles/index'
+
+  resources :articles
+  
   root 'articles#index'
 end
