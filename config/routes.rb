2390a14913f13b8c3f376fc60a4d51c414e476e9
vonnenaut <danielashcom@gmail.com>
2020-06-16 20:14:14 +0700

    added comment model and controller and ability to create and delete comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'articles/index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   
   root 'articles#index'
 end
